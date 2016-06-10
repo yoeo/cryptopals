fc202e9a075325f18f6fd8a5a842055c3cc2dcd4
yoeo <yandaar@laposte.net>
2016-06-10 22:29:00 +0200

    Add test runner.

---
+++ run.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+
+filters = '-t ~slow'
+OptionParser.new do |opts|
+  opts.banner += ' [SET_NUMBER]'
+  opts.separator 'Tests the implementation of Cryptopals Crypto Challenges.'
+  opts.on('--slow', 'run slow tests, may take hours') { filters = '' }
+end.parse!
+test_set = ARGV.empty? ? '' : "test_set_#{ARGV[0]}"
+
+system("rspec -fd #{filters} spec/#{test_set}*")
