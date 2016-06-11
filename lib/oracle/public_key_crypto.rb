618f5a97b1db2ee226782aaad57085c5f4cc5f6d
yoeo <yandaar@laposte.net>
2016-06-11 14:24:34 +0200

    Simplify Echo oracle.

--- lib/oracle/public_key_crypto.rb
+++ lib/oracle/public_key_crypto.rb
@@ -5,107 +5,65 @@ require_relative 'common'
 module Oracle
   # Diffie-Hellman key exchange
   class Echo < Base
-    STATES = {
-      put: [:put_arguments, :put_key, :put_message, :put_response],
-      get: [:get_arguments, :get_key, :get_message, :get_response]
-    }.freeze
+    STATE_MACHINE =
+      %w(arguments public_key message response authenticate).freeze
 
     attr_reader :authenticated
 
     def initialize
       @key_gen = nil
-      @remote_pub_key = 0
-      @state_i = 0
-      @message = %w(dog lama rat eagle frog lion lizad dolphin)[rand_i(8)]
-      @authenticated = true
+      @key_text = nil
+      @message = nil
+    end
+
+    def step(args = nil)
+      step_i, step_args = args || [0, []]
+      puts "#{self}: #{step_i}/#{STATE_MACHINE[step_i]}: #{step_args}"
+      [step_i + 1, send(STATE_MACHINE[step_i], *step_args)]
     end
 
     def encrypt
       iv_text = rand_text(16)
-      session_key = @key_gen.compute_key(@remote_pub_key)
-      key_text = OpenSSL::Digest::SHA1.digest(session_key)[0...16]
-      [aes_encrypt(:CBC, @message, key_text, iv_text), iv_text]
+      [aes_encrypt(:CBC, @message, @key_text, iv_text), iv_text]
     end
 
     def decrypt(text, iv_text)
-      session_key = @key_gen.compute_key(@remote_pub_key)
-      key_text = OpenSSL::Digest::SHA1.digest(session_key)[0...16]
-      aes_decrypt(:CBC, text, key_text, iv_text)
+      aes_decrypt(:CBC, text, @key_text, iv_text)
     end
 
-    # send data
+    # state machine steps
 
-    def put_arguments
+    def arguments
       @key_gen = OpenSSL::PKey::DH.new(1024)
       [@key_gen.public_key.to_der, @key_gen.pub_key]
     end
 
-    def put_key
+    def public_key(der, remote_pub_key)
+      @key_gen = OpenSSL::PKey::DH.new(der)
+      @key_gen.generate_key!
+      session_key = @key_gen.compute_key(remote_pub_key)
+      @key_text = OpenSSL::Digest::SHA1.digest(session_key)[0...16]
       [@key_gen.pub_key]
     end
 
-    alias put_message encrypt
-    alias put_response encrypt
-
-    def put
-      puts "put #{self} => #{@state_i}"
-      @state_i += 1
-      send(STATES[:put][@state_i - 1])
+    def message(remote_pub_key)
+      session_key = @key_gen.compute_key(remote_pub_key)
+      @key_text = OpenSSL::Digest::SHA1.digest(session_key)[0...16]
+      @message = %w(lama rat dolphin)[rand_i(3)]
+      encrypt
     end
 
-    # receive data
-
-    def get_arguments(der, remote_pub_key)
-      get_key(remote_pub_key)
-      @key_gen = OpenSSL::PKey::DH.new(der)
-      @key_gen.generate_key!
-    end
-
-    def get_key(remote_pub_key)
-      @remote_pub_key = remote_pub_key
-    end
-
-    def get_message(encrypted_text, iv_text)
+    def response(encrypted_text, iv_text)
       @message = "I'm a #{decrypt(encrypted_text, iv_text)}"
+      encrypt
     end
 
-    def get_response(encrypted_text, iv_text)
-      @authenticated = decrypt(encrypted_text, iv_text) == "I'm a #{@message}"
-    end
-
-    def get(args)
-      puts "get #{self} => #{@state_i} | #{args.inspect}"
-      send(STATES[:get][@state_i], *args)
-      @state_i += 1
+    def authenticate(encrypted_text, iv_text)
+      decrypt(encrypted_text, iv_text) == "I'm a #{@message}"
     end
   end
 
   # Inject parameters durring Diffie-Hellman key exchange
   class EchoInjecter < Echo
-    attr_reader :forged
-
-    # send data
-
-    alias put_arguments forged
-    alias put_key forged
-    alias put_message forged
-    alias put_response forged
-
-    # receive data
-
-    def get_arguments(der, _remote_pub_key)
-      @key_gen = OpenSSL::PKey::DH.new(der)
-      @forged = [der, @key_gen.p]
-    end
-
-    def get_key2(_remote_pub_key)
-      @forged = [@key_gen.p]
-    end
-
-    def get_message(encrypted_text, iv_text)
-      @forged = [encrypted_text, iv_text]
-    end
-
-    alias get_message get_response
   end
 end
