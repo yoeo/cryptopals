4b3d06268f647e40facc5b8f2f96e1c51eb099e7
yoeo <yandaar@laposte.net>
2016-06-19 13:40:59 +0200

    Reorder the required modules.

--- lib/impl.rb
+++ lib/impl.rb
@@ -1,6 +1,7 @@
 # Implementation of cryptography algorithms
+require_relative 'impl/diffie_hellman'
 require_relative 'impl/hmac'
 require_relative 'impl/md4'
 require_relative 'impl/mt19973'
+require_relative 'impl/secure_remote_password'
 require_relative 'impl/sha1'
-require_relative 'impl/diffie_hellman'
