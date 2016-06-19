44f0c25ee1ba60140847fec4a49c21b1aeae3eb9
yoeo <yandaar@laposte.net>
2016-06-19 18:31:18 +0200

    Set 5 test 37.

--- spec/test_set_5_diffie_hellman_and_friends.rb
+++ spec/test_set_5_diffie_hellman_and_friends.rb
@@ -1,6 +1,16 @@
 require 'set_5_diffie_hellman_and_friends'
 
 RSpec.describe DiffieHellmanAndFriends do
+  NIST_PRIME =
+    'ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024' \
+    'e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd' \
+    '3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec' \
+    '6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f' \
+    '24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361' \
+    'c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552' \
+    'bb9ed529077096966d670c354e4abc9804f1746c08ca237327fff' \
+    'fffffffffffff'.to_i(16)
+
   describe '33. Implement Diffie-Hellman' do
     it 'ensures that the Diffie-Hellman session keys are valid' do
       expect(DiffieHellmanAndFriends.valid_dh_session_key).to be true
@@ -23,16 +33,7 @@ RSpec.describe DiffieHellmanAndFriends do
     '35. Implement DH with negotiated groups, and break with malicious "g" ' \
     'parameters'
   describe description_35 do
-    P =
-      'ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024' \
-      'e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd' \
-      '3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec' \
-      '6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f' \
-      '24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361' \
-      'c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552' \
-      'bb9ed529077096966d670c354e4abc9804f1746c08ca237327fff' \
-      'fffffffffffff'.to_i(16)
-
+    P = NIST_PRIME
     it 'checks the negotiated groups protocol based on Diffie-Hellman' do
       expect(DiffieHellmanAndFriends.dh_negotiated_group_working[1]).to be true
     end
@@ -69,7 +70,8 @@ RSpec.describe DiffieHellmanAndFriends do
         matching_credentials.all? do |server_credentials, client_credentials|
           DiffieHellmanAndFriends.check_secure_remote_password(
             server_credentials, client_credentials)[1]
-        end).to be true
+        end
+      ).to be true
     end
 
     it "fails to authenticate when credentials doesn't match" do
@@ -77,7 +79,25 @@ RSpec.describe DiffieHellmanAndFriends do
         unmatching_credentials.none? do |server_credentials, client_credentials|
           DiffieHellmanAndFriends.check_secure_remote_password(
             server_credentials, client_credentials)[1]
-        end).to be true
+        end
+      ).to be true
+    end
+  end
+
+  describe '37. Break SRP with a zero key' do
+    N = NIST_PRIME
+    it 'confirms that session value is 0 when injected client key is 0' do
+      expect(DiffieHellmanAndFriends.malicious_srp_client_key(0)[1]).to be true
+    end
+
+    it 'confirms that session value is 0 when injected client key is N' do
+      expect(DiffieHellmanAndFriends.malicious_srp_client_key(N)[1]).to be true
+    end
+
+    it 'confirms that session value is 0 when injected client key is x * N' do
+      x = (0..10).to_a.sample
+      expect(
+        DiffieHellmanAndFriends.malicious_srp_client_key(x * N)[1]).to be true
     end
   end
 end
