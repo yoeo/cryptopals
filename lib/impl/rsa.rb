11d4c302ced48e05207568fd8107cf8bff180e46
yoeo <yandaar@laposte.net>
2016-06-26 14:28:45 +0200

    Patch OpenSLL big numbers, move methods to class level.

--- lib/impl/rsa.rb
+++ lib/impl/rsa.rb
@@ -1,5 +1,14 @@
 require 'openssl'
 
+module OpenSSL
+  # Patch OpenSSL::BN, add integer division
+  class BN
+    def div(denominator)
+      (self / denominator)[0]
+    end
+  end
+end
+
 module Impl
   # RSA cryptosystem implementation
   class RSA
@@ -9,26 +18,40 @@ module Impl
       totient = (p_prime - 1) * (q_prime - 1)
       @n_prime = p_prime * q_prime
       @e_value = 3
-      @d_value = invmod(@e_value, totient)
+      @d_value = cls.invmod(@e_value, totient)
     end
 
-    def extended_gcd(a, n)
+    def cls
+      self.class
+    end
+
+    def self.extended_gcd(a, n)
       t = 0
       new_t = 1
       r = n
       new_r = a
       loop do
         return [r, t] if new_r == 0
-        quotient = (r.to_bn / new_r)[0]
+        quotient = r.div(new_r)
         t, new_t = new_t, t - quotient * new_t
         r, new_r = new_r, r - quotient * new_r
       end
     end
 
-    def invmod(a, n)
+    def self.invmod(a, n)
       r, t = extended_gcd(a, n)
       raise 'the modulo is not invertible' unless r == 1
       t + (t < 0 ? n : 0)
+    end
+
+    def self.to_value(text)
+      text.unpack('H*')[0].to_i(16)
+    end
+
+    def self.to_text(value)
+      hex_text = value.to_s(16)
+      hex_text = '0' + hex_text if hex_text.length.odd?
+      [hex_text].pack('H*').force_encoding('utf-8')
     end
 
     def public_key
@@ -40,21 +63,12 @@ module Impl
     end
 
     def encrypt(text)
-      to_value(text).to_bn.mod_exp(*public_key)
+      cls.to_value(text).to_bn.mod_exp(*public_key)
     end
 
     def decrypt(value)
-      to_text(value.to_bn.mod_exp(*private_key))
+      cls.to_text(value.to_bn.mod_exp(*private_key))
     end
 
-    def to_value(text)
-      text.unpack('H*')[0].to_i(16)
-    end
-
-    def to_text(value)
-      hex_text = value.to_s(16)
-      hex_text = '0' + hex_text if hex_text.length.odd?
-      [hex_text].pack('H*').force_encoding('utf-8')
-    end
   end
 end
