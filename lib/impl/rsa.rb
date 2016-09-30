0a471c03b6332069c1717ef12c213b74f8d599c8
yoeo <yandaar@laposte.net>
2016-09-30 19:50:46 +0200

    Updated set 5.

--- lib/impl/rsa.rb
+++ lib/impl/rsa.rb
@@ -1,47 +1,23 @@
 require 'openssl'
 
-module OpenSSL
-  # Patch OpenSSL::BN, add integer division
-  class BN
-    def div(denominator)
-      (self / denominator)[0]
-    end
-  end
-end
+require_relative 'common'
 
 module Impl
   # RSA cryptosystem implementation
   class RSA
+    include Modulo
+
     def initialize(key_size = 1024)
       p_prime = OpenSSL::BN.generate_prime(key_size)
       q_prime = OpenSSL::BN.generate_prime(key_size)
       totient = (p_prime - 1) * (q_prime - 1)
       @n_prime = p_prime * q_prime
       @e_value = 3
-      @d_value = cls.invmod(@e_value, totient)
+      @d_value = invmod(@e_value, totient)
     end
 
     def cls
       self.class
-    end
-
-    def self.extended_gcd(a, n)
-      t = 0
-      new_t = 1
-      r = n
-      new_r = a
-      loop do
-        return [r, t] if new_r.zero?
-        quotient = r.div(new_r)
-        t, new_t = new_t, t - quotient * new_t
-        r, new_r = new_r, r - quotient * new_r
-      end
-    end
-
-    def self.invmod(a, n)
-      r, t = extended_gcd(a, n)
-      raise 'the modulo is not invertible' unless r == 1
-      t + (t < 0 ? n : 0)
     end
 
     def self.to_value(text)
@@ -68,13 +44,13 @@ module Impl
 
     def encrypt(text)
       value = cls.to_value(text)
-      result_text = value.to_bn.mod_exp(*public_key)
+      result_text = mod_exp(value, *public_key)
       cls.to_text(result_text)
     end
 
     def decrypt(text)
       value = cls.to_value(text)
-      result_text = value.to_bn.mod_exp(*private_key)
+      result_text = mod_exp(value, *private_key)
       cls.to_text(result_text)
     end
   end
