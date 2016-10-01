32dee1c4dab26625efe27e33a5fe89a4fd3bf8f6
yoeo <yandaar@laposte.net>
2016-10-01 16:50:22 +0200

    Callenge 44 tests.

--- spec/test_set_6_rsa_and_dsa.rb
+++ spec/test_set_6_rsa_and_dsa.rb
@@ -52,4 +52,20 @@ RSpec.describe RSAAndDSA do
       expect(RSAAndDSA.dsa_key_recovered(message, r, s, y)).to eq(x_hash)
     end
   end
+
+  describe '44: DSA nonce recovery from repeated nonce' do
+    messages_file = 'data/44.txt'
+    y =
+      '2d026f4bf30195ede3a088da85e398ef869611d0f68f07' \
+      '13d51c9c1a3a26c95105d915e2d8cdf26d056b86b8a7b8' \
+      '5519b1c23cc3ecdc6062650462e3063bd179c2a6581519' \
+      'f674a61f1d89a1fff27171ebc1b93d4dc57bceb7ae2430' \
+      'f98a6a4d83d8279ee65d71c1203d2c96d65ebbf7cce9d3' \
+      '2971c3de5084cce04a2e147821'.to_i(16)
+    x_hash = 'ca8f6f7c66fa362d40760d135b763eb8527d3d52'
+
+    it 'recovers DSA secret key x from reused session key k' do
+      expect(RSAAndDSA.reused_key_recovery(messages_file)).to eq([x_hash, y])
+    end
+  end
 end
