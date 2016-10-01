e6db00d2a4ce6edd34fdcce63c5a07e1ae741b91
yoeo <yandaar@laposte.net>
2016-10-01 16:49:53 +0200

    Challenge 44 answer.

--- lib/set_6_rsa_and_dsa.rb
+++ lib/set_6_rsa_and_dsa.rb
@@ -92,16 +92,51 @@ module RSAAndDSA
     Impl::DSA::G.to_bn.mod_exp(x, Impl::DSA::P).to_i
   end
 
+  def make_private_key(h, k, r, s)
+    Impl::Modulo.invmod(r, Impl::DSA::Q) * (s * k - h) % Impl::DSA::Q
+  end
+
   def recover_private_key(message, r, s, y)
     h = Digest::SHA1.hexdigest(message).to_i(16)
-    q = Impl::DSA::Q
     (2**16).times do |k|
-      x = Impl::Modulo.invmod(r, q) * (s * k - h) % q
+      x = make_private_key(h, k, r, s)
       return x if make_public_key(x) == y
     end
   end
 
+  def to_sha1(value)
+    Digest::SHA1.hexdigest(value.to_s(16))
+  end
+
   def dsa_key_recovered(message, r, s, y)
-    Digest::SHA1.hexdigest(recover_private_key(message, r, s, y).to_s(16))
+    to_sha1(recover_private_key(message, r, s, y))
+  end
+
+  # 44. Recover DSA private key from reused session key
+
+  def read_signatures(filename)
+    File.read(filename).each_line.each_slice(4).map do |lines|
+      [10, 10, 16].each_with_index.map do |base, index|
+        lines[index + 1][3..-1].to_i(base)
+      end.to_a
+    end
+  end
+
+  def compute_keys(first_signature, second_signature)
+    s_one, r, m_one = *first_signature
+    s_two, _, m_two = *second_signature
+    q = Impl::DSA::Q
+    k = Impl::Modulo.invmod(s_one - s_two, q) * (m_one - m_two) % q
+    x = make_private_key(m_one, k, r, s_one)
+    [x, make_public_key(x)]
+  end
+
+  def reused_key_recovery(filename)
+    data = read_signatures(filename)
+    r_values = data.map { |e| e[1] }
+    r = r_values.select { |e| r_values.count(e) > 1 }[0]
+    signatures = data.select { |e| e[1] == r }[0..2]
+    x, y = compute_keys(*signatures)
+    [to_sha1(x), y]
   end
 end
